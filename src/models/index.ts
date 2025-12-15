2e527e0f2ced491579aee9992f8099200075c0cd
thiengl2015 <giathien899@gmail.com>
2025-12-15 13:30:36 +0700

    feat(models): export Permission and RolePermission models
    - Add Permission and RolePermission imports to index
    - Export all models from central barrel file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,7 @@
 import sequelize from "../config/database";
 import User from "./User";
 import Role from "./Role";
+import Permission from "./Permission";
+import RolePermission from "./RolePermission";
 
-export { sequelize, User, Role };
+export { sequelize, User, Role, Permission, RolePermission };
